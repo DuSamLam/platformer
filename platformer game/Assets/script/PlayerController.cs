4df36d717aa930731d05f7637b64eaec2e594605
DuSamLam <102325897+DuSamLam@users.noreply.github.com>
2023-11-07 13:47:40 -0500

    c# script modifications

--- platformer game/Assets/script/PlayerController.cs
+++ platformer game/Assets/script/PlayerController.cs
@@ -6,53 +6,49 @@ using UnityEngine.SceneManagement; //importing SceneManagement library
 public class PlayerController : MonoBehaviour
 {
     public float speed = 0.5f;
-    public bool hasKey = false;
+    SpriteRenderer sprite;
+    public float jumpForce; //the force that will be added to the verical component of player's velocity
+    Rigidbody2D rb; //create reference for rigidbody bc jump requires physics
 
-    public GameObject key;
+    //Ground check Variables
+    public LayerMask groundLayer;
+    
 
     public static PlayerController instance; //creating an object of the class to be findable
 
     // Start is called before the first frame update
     void Start()
     {
-        if (instance != null) //check if instance is in the scene
-        {
-            Destroy(gameObject);
-            return;
-        }
-        instance = this;
-        GameObject.DontDestroyOnLoad(gameObject);
+        rb = GetComponent<Rigidbody2D>();
+        sprite = GetComponent<SpriteRenderer>();
     }
 
     // Update is called once per frame
     void Update()
     {
         Vector3 newPosition = transform.position;
-
-        if (Input.GetKey("w"))
-        {
-            //player moves up
-            newPosition.y += speed;
-        }
-
-        if (Input.GetKey("s"))
-        {
-            //player moves down
-            newPosition.y -= speed;
-        }
+        Vector3 newScale = transform.localScale;
+        float currentScale = Mathf.Abs(transform.localScale.x);
 
         //player moves left
-        if (Input.GetKey("a"))
+        if (Input.GetKey("a") || Input.GetKey(KeyCode.LeftArrow))
         {
             newPosition.x -= speed;
+            newScale.x = -currentScale;
         }
 
         //player moves right
-        if (Input.GetKey("d"))
+        if (Input.GetKey("d") || Input.GetKey(KeyCode.RightArrow))
         {
             newPosition.x += speed;
+            newScale.x = currentScale;
         }
 
+        if (Input.GetKey("w") || Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+        }
         transform.position = newPosition;
+        transform.localScale = newScale;
     }
 }
